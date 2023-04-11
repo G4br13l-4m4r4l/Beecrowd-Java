16870ac0f65ea8a6f089856182d8af64e900800e
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-04-10 22:18:24 -0300

    Startint with the logic of 1018

---
+++ logics/src/beginner1018/Cedulas.java
@@ -0,0 +1,20 @@
+package beginner1018;
+
+import java.util.Scanner;
+public class Cedulas {
+	public static void main (String[] args) {
+		
+		/**
+		 * Leia um valor inteiro. 
+		 * A seguir, calcule o menor número de notas possíveis (cédulas) no qual 
+		 * o valor pode ser decomposto. As notas consideradas são de 100, 50, 20, 10, 5, 2 e 1. 
+		 * A seguir mostre o valor lido e a relação de notas necessárias.
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		int money = input.nextInt();
+		
+		
+		input.close();
+	}
+}
