a161b59d1a280908f3e3d27655f684e296a046cf
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-04-17 21:08:59 -0300

    Adding logic 1021 and starting 1035

--- logics/src/beginner1021/Notas_moedas.java
+++ logics/src/beginner1021/Notas_moedas.java
@@ -12,7 +12,58 @@ public class Notas_moedas {
 		 * A seguir mostre a relação de notas necessárias.
 		 */
 		Scanner input = new Scanner(System.in);
+		double money = input.nextDouble();
+		System.out.println("NOTAS:");
 		
+		int cem = (int) money/100;
+		money = money%100.00;
+		System.out.println(cem+" nota(s) de R$ 100.00");
+		
+		int cinquenta =(int) money/50;
+		money = money%50.00;
+		System.out.println(cinquenta+" nota(s) de R$ 50.00");
+		
+		int vinte =(int) money/20;
+		money = money%20.00;
+		System.out.println(vinte+" nota(s) de R$ 20.00");
+		
+		int dez =(int) money/10;
+		money = money%10.00;
+		System.out.println(dez+" nota(s) de R$ 10.00");
+		
+		int cinco =(int) money/5;
+		money = money%5.00;
+		System.out.println(cinco+" nota(s) de R$ 5.00");
+		
+		int dois = (int)money/2;
+		money = money%2.00;
+		System.out.println(dois+" nota(s) de R$ 2.00");
+		
+		money = money*100;
+		System.out.println("MOEDAS:");
+		
+		int um = (int)money/100;
+		money = money%100.00;
+		System.out.println(um+" moeda(s) de R$ 1.00");
+		
+		int cinqCent = (int) money/50;
+		money = money%50.00;
+		System.out.println(cinqCent+" moeda(s) de R$ 0.50");
+		
+		int vinteCinco = (int)money/25;
+		money = money%25.00;
+		System.out.println(vinteCinco+" moeda(s) de R$ 0.25");
+		
+		int dezCent = (int) money/10;
+		money = money%10.00;
+		System.out.println(dezCent+" moeda(s) de R$ 0.10");
+		
+		int cincoCent = (int)money/5;
+		money = money%5.00;
+		System.out.println(cincoCent+" moeda(s) de R$ 0.05");
+		
+		int umCent = (int)money/1;
+		System.out.println(umCent+" moeda(s) de R$ 0.01");
 		input.close();
 	}
 }
