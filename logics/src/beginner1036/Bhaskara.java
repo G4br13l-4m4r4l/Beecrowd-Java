a7c25450d1c090bfbc6475fda968ffa78c90865d
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-04-21 10:48:32 -0300

    Adding logic 1036 and starting 1037

--- logics/src/beginner1036/Bhaskara.java
+++ logics/src/beginner1036/Bhaskara.java
@@ -7,12 +7,23 @@ public class Bhaskara {
 		 * Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. 
 		 * Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, 
 		 * caso haja uma divisão por 0 ou raiz de numero negativo.
+		 * x = (-b ± √(b² – 4ac)) / (2a)
 		 */
 		Scanner input = new Scanner(System.in);
 		double A = input.nextDouble();
 		double B = input.nextDouble();
 		double C = input.nextDouble();
 		
+		double delta = Math.pow(B, 2)-(4*A*C);
+		double R1 = (-B + Math.sqrt(delta))/(2*A);
+		double R2 = (-B - Math.sqrt(delta))/(2*A);
+		
+		if(delta>0 && A!=0) {
+			System.out.printf("R1 = %.5f%n", R1);
+			System.out.printf("R2 = %.5f%n", R2);
+		}else {
+			System.out.println("Impossivel calcular");
+		}
 		input.close();
 	}
 }
---
+++ logics/src/beginner1037/Intervalo.java
@@ -0,0 +1,13 @@
+package beginner1037;
+
+import java.util.Scanner;
+public class Intervalo {
+	public static void main(String[] args) {
+		/**
+		 * 
+		 */
+		Scanner input = new Scanner(System.in);
+		
+		input.close();
+	}
+}
