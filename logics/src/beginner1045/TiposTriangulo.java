0ed320e2b6ee9473ddc2f159a0a97ba60edb5096
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-04 20:57:46 -0300

    adding logic 1045 with some upgrade

--- logics/src/beginner1045/TiposTriangulo.java
+++ logics/src/beginner1045/TiposTriangulo.java
@@ -15,7 +15,28 @@ public class TiposTriangulo {
 		Scanner input = new Scanner(System.in);
 		
 		try {
+			double a = input.nextDouble();
+			double b = input.nextDouble();
+			double c = input.nextDouble();
 			
+			double A = Math.max(a, Math.max(b, c));
+			double C = Math.min(a, Math.min(b, c));
+			double B = Math.min(a, Math.max(a, b));
+			
+			if(B+C<=A) {
+				System.out.println("NAO FORMA TRIANGULO");
+			}else if(Math.pow(A, 2)==Math.pow(B, 2)+Math.pow(C, 2)) {
+				System.out.println("TRIANGULO RETANGULO");
+			}else if(Math.pow(A, 2)>Math.pow(B, 2)+Math.pow(C, 2)) {
+				System.out.println("TRIANGULO OBTUSANGULO");
+			}else if(Math.pow(A, 2)<Math.pow(B, 2)+Math.pow(C, 2)) {
+				System.out.println("TRIANGULO ACUTANGULO");
+			}
+			if(A==B && B==C && A==C) {
+				System.out.println("TRIANGULO EQUILATERO");
+			}else if(A==B || B==C || A==C) {
+				System.out.println("TRIANGULO ISOSCELES");
+			}
 
 		}catch(Exception e) {
 			System.out.println(e.getMessage());
