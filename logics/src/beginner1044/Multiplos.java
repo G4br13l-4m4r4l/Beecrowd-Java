407220d38c2e3081d7a8369fd7fa9e45642ce553
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-03 22:19:50 -0300

    adding logic 1044 and starting 1045

--- logics/src/beginner1044/Multiplos.java
+++ logics/src/beginner1044/Multiplos.java
@@ -12,7 +12,11 @@ public class Multiplos {
 		int A = input.nextInt();
 		int B = input.nextInt();
 		
-		
+		if(B%A == 0 || A%B == 0) {
+			System.out.println("Sao Multiplos");
+		}else {
+			System.out.println("Nao sao Multiplos");
+		}
 		input.close();
 	}
 }
---
+++ logics/src/beginner1045/TiposTriangulo.java
@@ -0,0 +1,27 @@
+package beginner1045;
+
+import java.util.Scanner;
+public class TiposTriangulo {
+	public static void main(String[] args) {
+		/**
+		 * Leia 3 valores de ponto flutuante A, B e C e 
+		 * ordene-os em ordem decrescente, de modo que o 
+		 * lado A representa o maior dos 3 lados. 
+		 * A seguir, determine o tipo de triângulo que estes 
+		 * três lados formam, com base nos seguintes casos, 
+		 * sempre escrevendo uma mensagem adequada
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		
+		try {
+			
+
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}finally {
+			input.close();
+		}		
+		
+	}
+}
