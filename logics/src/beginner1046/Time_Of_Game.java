053b72bbc89e181d115b867d5b0a28ac825290ae
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-05 12:58:43 -0300

    Starting logic 1046

---
+++ logics/src/beginner1046/Time_Of_Game.java
@@ -0,0 +1,19 @@
+package beginner1046;
+
+import java.util.Scanner;
+public class Time_Of_Game {
+	public static void main(String[] args) {
+		/**
+		 * Leia a hora inicial e a hora final de um jogo. 
+		 * A seguir calcule a duração do jogo, sabendo que o mesmo 
+		 * pode começar em um dia e terminar em outro, 
+		 * tendo uma duração mínima de 1 hora e máxima de 24 horas.
+		 */
+		Scanner input = new Scanner(System.in);
+		int inicio = input.nextInt();
+		int fim = input.nextInt();
+		
+		
+		input.close();
+	}
+}
