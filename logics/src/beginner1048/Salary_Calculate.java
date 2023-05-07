871f3e32c9c1ad8eb91081fe126a11b4ea386fba
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-07 12:35:37 -0300

    Adding logic 1048 and starting 1049

--- logics/src/beginner1048/Salary_Calculate.java
+++ logics/src/beginner1048/Salary_Calculate.java
@@ -17,8 +17,28 @@ public class Salary_Calculate {
 		 */
 		Scanner input = new Scanner(System.in);
 		double salario = input.nextDouble();
-		
-		
+		double aumento;
+		if(salario >= 0.0 && salario <=400.00) {
+			aumento = salario * 0.15;
+			double salarioNovo = salario + aumento;
+			System.out.printf("Novo salario: %.2f%nReajuste ganho: %.2f%nEm percentual: 15  %n", salarioNovo, aumento);
+		}else if(salario>=400.01 && salario<=800.00) {
+			aumento = salario * 0.12;
+			double salarioNovo = salario + aumento;
+			System.out.printf("Novo salario: %.2f%nReajuste ganho: %.2f%nEm percentual: 12 % %n", salarioNovo, aumento);
+		}else if(salario>=800.01 && salario<=1200.00) {
+			aumento = salario * 0.10;
+			double salarioNovo = salario + aumento;
+			System.out.printf("Novo salario: %.2f%nReajuste ganho: %.2f%nEm percentual: 10 % %n", salarioNovo, aumento);
+		}else if(salario>=1200.01 && salario<=2000.00) {
+			aumento = salario * 0.7;
+			double salarioNovo = salario + aumento;
+			System.out.printf("Novo salario: %.2f%nReajuste ganho: %.2f%nEm percentual: 7 % %n", salarioNovo, aumento);
+		}else {
+			aumento = salario * 0.4;
+			double salarioNovo = salario + aumento;
+			System.out.printf("Novo salario: %.2f%nReajuste ganho: %.2f%nEm percentual: 4 % %n", salarioNovo, aumento);
+		}
 		input.close();
 	}
 }
---
+++ logics/src/beginner1049/Animal.java
@@ -0,0 +1,20 @@
+package beginner1049;
+
+import java.util.Scanner;
+public class Animal {
+	public static void main(String[] args) {
+		/**
+		 * este problema, você deverá ler 3 palavras que definem o tipo de animal 
+		 * possível segundo o esquema abaixo, da esquerda para a direita.  
+		 * Em seguida conclua qual dos animais seguintes foi escolhido,
+		 * através das três palavras fornecidas.
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		String first = input.nextLine();
+		String second = input.nextLine();
+		String third = input.nextLine();
+		
+		input.close();
+	}
+}
