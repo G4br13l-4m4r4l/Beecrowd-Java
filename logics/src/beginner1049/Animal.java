39da245af1f9d0ac2412d569ba5d4bfe1b743765
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-09 20:46:00 -0300

    adding logic 1048 1049 and starting 1050

--- logics/src/beginner1049/Animal.java
+++ logics/src/beginner1049/Animal.java
@@ -15,6 +15,37 @@ public class Animal {
 		String second = input.nextLine();
 		String third = input.nextLine();
 		
+		if(first.trim().toLowerCase().equals("veterbrado")) {
+			if(second.trim().toLowerCase().equals("ave")) {
+				if(third.trim().toLowerCase().equals("onivoro")) {
+					System.out.println("pomba");
+				}else {
+					System.out.println("aguia");
+				}
+			
+			}else {
+				if(third.trim().toLowerCase().equals("onivoro")) {
+					System.out.println("homem");
+				}else{
+					System.out.println("vaca");
+				}
+				
+			}
+		}else{
+			if(second.trim().toLowerCase().equals("inseto")) {
+				if(third.trim().toLowerCase().equals("hematofago")) {
+					System.out.println("pulga");
+				}else {
+					System.out.println("lagarta");
+				}
+			}else {
+				if(third.trim().toLowerCase().equals("hematofago")) {
+					System.out.println("sanguessuga");
+				}else {
+					System.out.println("minhoca");
+				}
+			}
+		}
 		input.close();
 	}
 }
---
+++ logics/src/beginner1050/DDD.java
@@ -0,0 +1,21 @@
+package beginner1050;
+
+import java.util.Scanner;
+public class DDD {
+	public static void main(String[] args) {
+		
+		/**
+		 * Leia um número inteiro que representa um código de DDD para discagem interurbana. 
+		 * Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
+		 * 
+		 * Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, 
+		 * o programa deverá informar: DDD nao cadastrado
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		int ddd = input.nextInt();
+		
+		
+		input.close();
+	} 
+}
