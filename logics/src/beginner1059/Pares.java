68e602da169275e5571a5a4717bd7616c2ab144c
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-16 20:45:00 -0300

    adding logic of 1052 and starting 1059

---
+++ logics/src/beginner1059/Pares.java
@@ -0,0 +1,14 @@
+package beginner1059;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pares {
+	public static void main(String[] args) {
+		
+		/**
+		 * Faça um programa que mostre os números pares entre 1 e 100, inclusive.
+		 */
+		
+	}
+}
