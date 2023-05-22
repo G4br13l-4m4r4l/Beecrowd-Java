c1e8088ed244a6761c7336620fa924f9e958172d
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-22 20:46:40 -0300

    adding logic 1051

--- logics/src/beginner1051/Imposto_de_Renda.java
+++ logics/src/beginner1051/Imposto_de_Renda.java
@@ -16,18 +16,25 @@ public class Imposto_de_Renda {
 		 */
 		Scanner input = new Scanner(System.in);
 		double salario = input.nextDouble();
-		double imposto, diferenca;
+		double imposto=0, diferenca;
 		
-		if(salario <= 2000.00 && salario >= 0.0) {
-			System.out.println("Isento");
-		}else if(salario <= 2000.01 && salario >= 3000.00) {
-			
-		}else if(salario <= 3000.01 && salario >= 4500.00) {
-			
-		}else{
+		if(salario >4500.00) {
 			imposto = (1000.00*0.08)+(1500.00*0.18);
 			diferenca = salario -4500.00;
 			imposto+= diferenca*0.28;
+		}else if(salario < 4500.00 && salario > 3000.01) {
+			imposto = (1000.00*0.08);
+			diferenca = salario -3000.00;
+			imposto+= diferenca*0.18;
+		}else if(salario > 2000.01 && salario <= 3000.00) {
+			diferenca = salario -2000.00;
+			imposto+= diferenca*0.08;
+		}
+		
+		if(imposto>0.00){
+			System.out.printf("R$ %.2f\n", imposto);
+		} else {
+			System.out.println("Isento");
 		}
 		input.close();
 	}
