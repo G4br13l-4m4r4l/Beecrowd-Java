0675972085fe3b92ca78551898997e8203371afc
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-23 20:31:17 -0300

    starting logic 1064

---
+++ logics/src/beginner1064/Positivos_media.java
@@ -0,0 +1,18 @@
+package beginner1064;
+
+import java.util.Scanner;
+
+public class Positivos_media {
+	public static void main(String[] args) {
+		/**
+		 * Leia 6 valores. Em seguida, 
+		 * mostre quantos destes valores digitados foram positivos. Na próxima linha, 
+		 * deve-se mostrar a média de todos os valores positivos digitados, 
+		 * com um dígito após o ponto decimal.
+		 */
+		
+		Scanner input = new Scanner(System.in);
+		
+		input.close();
+	}
+}
