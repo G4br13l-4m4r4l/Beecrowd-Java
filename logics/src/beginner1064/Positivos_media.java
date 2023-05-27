215fb40562cb247d8b773f31b94649b227eb8105
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-27 20:16:54 -0300

    adding logic 1064

--- logics/src/beginner1064/Positivos_media.java
+++ logics/src/beginner1064/Positivos_media.java
@@ -13,6 +13,22 @@ public class Positivos_media {
 		
 		Scanner input = new Scanner(System.in);
 		
+		double media =0;
+		int count = 0;
+		
+		for(int i =0;i<6;i++) {
+			double N = input.nextDouble();
+			
+			if(N<0) {
+				count++;
+				media+=N;
+			}
+		}
+		
+		media = media/count;
+		
+		System.out.println(count+" valores positivos");
+		System.out.printf("%.1f\n", media);
 		input.close();
 	}
 }
