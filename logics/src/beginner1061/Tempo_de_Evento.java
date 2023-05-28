eb93f7fd15045b8f32ef7a441498118257b7c6dd
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-28 18:03:42 -0300

    addin upgrade in the logic 1061

--- logics/src/beginner1061/Tempo_de_Evento.java
+++ logics/src/beginner1061/Tempo_de_Evento.java
@@ -20,8 +20,26 @@ public class Tempo_de_Evento {
 		String hf[] = input.nextLine().replaceAll(" ","").split(":");
 		
 		int d1 = Integer.parseInt(di[1]);
-		int h1 = Integer.parseInt(hi[1]);
+		int h1 = Integer.parseInt(hi[0]);
+		int m1 = Integer.parseInt(hi[1]);
 		int s1 = Integer.parseInt(hi[2]);
+		
+		int d2 = Integer.parseInt(df[1]);
+		int h2 = Integer.parseInt(hf[0]);
+		int m2 = Integer.parseInt(hf[1]);
+		int s2 = Integer.parseInt(hf[2]);
+		
+		int q1,q2, tempo;
+		
+		q1 = s1+m1*60+h1*3600+d1*86400;
+		q2 = s2+m2*60+h2*3600+d2*86400;
+		tempo = q2- q1;
+		
+		System.out.printf("%d dia(s)\n",(tempo/86400));
+		tempo = tempo%86400;
+		System.out.printf("%d hora(s)\n", (tempo/3600));
+		tempo = tempo%3600;
+		System.out.printf("%d segundo(s)\n", tempo);
 		input.close();
 	}
 }
