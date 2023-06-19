506836e9c0e64c779f96e2b55d04736d1ef3f7b9
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-06-19 13:22:27 -0300

    finishing the logic of 1065

--- logics/src/beginner1065/Pares_de_cinco.java
+++ logics/src/beginner1065/Pares_de_cinco.java
@@ -1,20 +1,22 @@
 package beginner1065;
 
-import java.util.ArrayList;
+
 import java.util.Scanner;
 
 public class Pares_de_cinco {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		ArrayList<Integer> lista = new ArrayList<>();
 		
-		for(int i=0;i<5;i++) {
-			lista.add(input.nextInt());
-		}
 		int cont =0;
-		lista.forEach();
+		for(int i=0;i<5;i++) {
+			int n = input.nextInt();
+			
+			if(n%2==0) {
+				cont++;
+			}
+		}
 		
-		
+		System.out.println(cont+" valores pares");
 		input.close();
 	}
 }
